e626fa6bf8b673328699f30820d479a85dae5add
Butters <k.butterfield08@gmail.com>
2020-10-09 08:20:53 -0500

    Updating

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+echo "<!DOCTYPE html>
+<html lang=''>
+<head>
+	<meta charset='utf-8'>
+	<title>Example Title</title>
+	<meta name='author' content='Your Name'>
+	<meta name='description' content='Example description'>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	<link rel='stylesheet' href='my.css'>
+    <link rel='icon' type='image/x-icon' href=''/>
+    <link rel='stylesheet' href='index.css'>
+
+</head>";
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- We are going to have the index page be the login, and from the login,
+The website will redirect them to the approiate site, employee, HR, and Manager -->
+<?php 
+include"header.php";
+?>
+
+<body>
+    <header class="top-part">
+        <div id="contactUs">
+                <!-- We are going to combine the rent and purchases into one page -->
+                <ul>
+                    <li><a href="#contact Us">Contact Us</a></li>
+                <ul>                
+            </div><!-- End of Navigation --> 
+        <img src="Images/logo2.jpeg">
+            <!-- <a href="http://example.com"><img src="Images/logo2.jpeg"></a> -->
+            <!-- <a href="http://example.com"><img src="Images/hr.png" /></a> -->
+
+	</header>
+
+<div class="middle-part">
+<h2>Welcome To The Employee Managment System</h2>
+<button type="button">Sign in</button>
+</div>
+
+
+
+</body>
+
+</html>
