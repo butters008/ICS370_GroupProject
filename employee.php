c05547bd2c0b88aaa210c43f64b8bb231cfac018
Butters <k.butterfield08@gmail.com>
2020-10-09 09:27:52 -0500

    Update employee.php

--- employee.php
+++ employee.php
@@ -1,5 +1,13 @@
 <?php include "header.php"; ?>
+<!-- Keith - What I envision for the employee page is just going to be a lot of POSTs 
+from the server and maybe a request form to change their address or have the ability to
+change that from their end.  Depending on company and company policy and how strict they want
+it to be.   -->
 
+<!-- Dynamically log in employee name -->
 <h3>Employee</h3>
+<section class="profile">
+
+</section>
 
 <?php include "footer.php"; ?>
